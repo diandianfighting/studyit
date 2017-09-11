014ead323f17f79fc7d65501166f91a007568974
diandian <diandian@qq.com>
2017-09-11 22:37:06 +0800

    项目基本结构搭建完成，requirejs也已经使用到了项目中

---
+++ views/static/js/dashboard/login.js
@@ -0,0 +1,40 @@
+define(["jquery", "cookie"], function($){
+    $(function(){
+      $("form").submit(function(e){
+            var userName = $("#tc_name").val();
+            var pass = $("#tc_pass").val();
+
+            if(userName.trim() == ""){
+                alert("请输入用户名");
+                return false;
+            }
+
+            if(pass.trim() == ""){
+                alert("请输入密码");
+                return false;
+            }
+
+            $.ajax({
+                url: "/api/login",
+                type: "post",
+                data: {
+                    tc_name: userName,
+                    tc_pass: pass
+                },
+                success: function(data){
+                    if(data.code == 200){
+
+                        $.cookie("userinfo", JSON.stringify(data.result), {expires: 365, path: "/"});
+
+
+                        location.href = "/";
+
+                        
+                    }
+                }
+            });
+
+            return false;
+        })
+    })
+});
